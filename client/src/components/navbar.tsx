5eea8ca158f2a21018007b7fa1d7472e7cef4fd5
jaswanthkumar2002 <58783291+jaswanthkumar2002@users.noreply.github.com>
2025-09-01 22:55:53 +0100

    Update navbar.tsx

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -37,7 +37,7 @@ export default function Navbar() {
             <img
               src={logo}
               alt="BlueHawk Group Logo"
-              className="w-50 h-30"/* grow by height */
+              className="w-30 h-12"/* grow by height */
               data-testid="logo-image"
               />
             {/* <span className="text-xl font-bold text-foreground">
